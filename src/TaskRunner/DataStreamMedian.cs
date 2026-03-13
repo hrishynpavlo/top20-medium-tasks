4bb77ce7d1e6df8a64d7e70a7833f7e1bcd66dc7
pavlo.hrishyn <pavlo.hrishyn.dev@gmail.com>
2026-03-14 00:40:35 +0100

    clean up

--- src/TaskRunner/DataStreamMedian.cs
+++ src/TaskRunner/DataStreamMedian.cs
@@ -2,41 +2,42 @@
 
 public class DataStreamMedian
 {
+    // First item is the smallest in the upper half
     private readonly BinaryHeap _minBucket = new (true);
+    
+    // First item is the biggest in the lower half
     private readonly BinaryHeap _maxBucket = new (false);
 
     public void AddNum(int num)
     {
-        var median = FindMedian();
-        if (num < median)
-        {
-            _minBucket.Add(num);
-        }
-        else
+        if (_maxBucket.Length == 0 || num <= _maxBucket.Peek())
         {
             _maxBucket.Add(num);
         }
-
-        if (_minBucket.Lenght > _maxBucket.Lenght + 1)
+        else
         {
-            _maxBucket.Add(_minBucket.Extract());
+            _minBucket.Add(num);
         }
 
-        if(_maxBucket.Lenght > _minBucket.Lenght + 1)
+        if(_maxBucket.Length > _minBucket.Length + 1)
         {
             _minBucket.Add(_maxBucket.Extract());
+        }
+        else  if (_minBucket.Length > _maxBucket.Length + 1)
+        {
+            _maxBucket.Add(_minBucket.Extract());
         }
     }
 
     public double FindMedian()
     {
-        return (_minBucket.Lenght, _maxBucket.Lenght) switch
+        return (_minBucket.Length, _maxBucket.Length) switch
         {
             (0, 0) => 0,
             (0, _) => _maxBucket.Peek(),
             (_, 0) => _minBucket.Peek(),
-            _ when _minBucket.Lenght == _maxBucket.Lenght => (_maxBucket.Peek() + _minBucket.Peek()) / 2d,
-            _ when _minBucket.Lenght > _maxBucket.Lenght => _minBucket.Peek(),
+            _ when _minBucket.Length == _maxBucket.Length => (_maxBucket.Peek() + _minBucket.Peek()) / 2d,
+            _ when _minBucket.Length > _maxBucket.Length => _minBucket.Peek(),
             _ => _maxBucket.Peek()
         };
     }
@@ -52,7 +53,7 @@ public class BinaryHeap(bool isMinHeap)
         var index = _heap.Count - 1;
         var parentIndex = (index - 1) / 2;
 
-        MoveUpAndSwap(index, parentIndex);
+        HeapifyUp(index, parentIndex);
     }
 
     public int Peek()
@@ -68,20 +69,18 @@ public class BinaryHeap(bool isMinHeap)
         _heap[0] = _heap[index];
         _heap.RemoveAt(index);
 
-        index = _heap.Count - 1;
-        var parentIndex = (index - 1) / 2;
-
-        MoveUpAndSwap(index, parentIndex);
+        HeapifyDown();
 
         return result;
     }
 
-    public int Lenght => _heap.Count;
+    public int Length => _heap.Count;
 
-    private void MoveUpAndSwap(int index, int parentIndex)
+    private void HeapifyUp(int index, int parentIndex)
     {
-        while ((isMinHeap && _heap[index] > _heap[parentIndex])
-               || (!isMinHeap && _heap[index] < _heap[parentIndex]))
+        while (index > 0 && 
+               ((isMinHeap && _heap[index] < _heap[parentIndex]) ||
+                (!isMinHeap && _heap[index] > _heap[parentIndex])))
         {
             // swap
             (_heap[parentIndex], _heap[index]) = (_heap[index], _heap[parentIndex]);
@@ -89,6 +88,40 @@ public class BinaryHeap(bool isMinHeap)
             // move up
             index = parentIndex;
             parentIndex = (index - 1) / 2;
+        }
+    }
+
+    private void HeapifyDown()
+    {
+        int index = 0, left = 1, right = 2;
+
+        while (left < _heap.Count)
+        {
+            int target;
+            if (right < _heap.Count)
+            {
+                target = isMinHeap
+                    ? (_heap[left] < _heap[right] ? left : right)
+                    : (_heap[left] > _heap[right] ? left : right);
+            }
+            else
+            {
+                target = left;
+            }
+
+            if ((isMinHeap && _heap[index] > _heap[target]) ||
+                (!isMinHeap && _heap[index] < _heap[target]))
+            {
+                (_heap[index], _heap[target]) = (_heap[target], _heap[index]);
+                
+                index = target;
+                left = index * 2 + 1;
+                right = index * 2 + 2;
+            }
+            else
+            {
+                break;
+            }
         }
     }
 }
