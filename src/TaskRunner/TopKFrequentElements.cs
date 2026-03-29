cbb874f397478a269850f617aab957226bf50573
pavlo.hrishyn <pavlo.hrishyn.dev@gmail.com>
2026-03-29 23:51:24 +0200

    resolved top k frequent elements

---
+++ src/TaskRunner/TopKFrequentElements.cs
@@ -0,0 +1,37 @@
+﻿namespace TaskRunner;
+
+public class TopKFrequentElements
+{
+    public int[] TopKFrequent(int[] nums, int k)
+    {
+        var map = new Dictionary<int, int>();
+        var heap = new PriorityQueue<int, int>();
+        
+        var result = new int[k];
+
+        foreach (var element in nums)
+        {
+            var frequency = map.GetValueOrDefault(element, 0) + 1;
+            map[element] = frequency;
+        }
+
+        foreach (var (element, frequency) in map)
+        {
+            if (heap.Count < k)
+            {
+                heap.Enqueue(element, frequency);
+            }
+            else if(heap.TryPeek(out _, out var minFrequency) && minFrequency < frequency)
+            {
+                heap.EnqueueDequeue(element, frequency);
+            }
+        }
+
+        for (int i = 0; i < result.Length; i++)
+        {
+            result[i] = heap.Dequeue();
+        }
+
+        return result;
+    }
+}
---
+++ src/Tester/TopKFrequentElementsTests.cs
@@ -0,0 +1,75 @@
+﻿using TaskRunner;
+
+namespace Tester;
+
+public class TopKFrequentElementsTests
+{
+    private readonly TopKFrequentElements _sut = new();
+
+    private static int[] Sorted(int[] arr) => arr.OrderBy(x => x).ToArray();
+
+    [Fact]
+    public void TopKFrequent_BasicCase_ReturnsTwoMostFrequent()
+    {
+        var result = _sut.TopKFrequent([1, 1, 1, 2, 2, 3], 2);
+
+        Assert.Equal([1, 2], Sorted(result));
+    }
+
+    [Fact]
+    public void TopKFrequent_KEquals1_ReturnsMostFrequent()
+    {
+        var result = _sut.TopKFrequent([3, 0, 1, 0], 1);
+
+        Assert.Equal([0], Sorted(result));
+    }
+
+    [Fact]
+    public void TopKFrequent_SingleElement_ReturnsThatElement()
+    {
+        var result = _sut.TopKFrequent([42], 1);
+
+        Assert.Equal([42], Sorted(result));
+    }
+
+    [Fact]
+    public void TopKFrequent_KEqualsArrayLength_ReturnsAllElements()
+    {
+        var result = _sut.TopKFrequent([1, 2, 3], 3);
+
+        Assert.Equal([1, 2, 3], Sorted(result));
+    }
+
+    [Fact]
+    public void TopKFrequent_NegativeNumbers_ReturnsCorrectElements()
+    {
+        var result = _sut.TopKFrequent([-1, -1, 2, 2, 2, 3], 2);
+
+        Assert.Equal([-1, 2], Sorted(result));
+    }
+
+    [Fact]
+    public void TopKFrequent_ClearWinner_ReturnsMostFrequent()
+    {
+        var result = _sut.TopKFrequent([4, 4, 4, 4, 1, 2, 3], 1);
+
+        Assert.Equal([4], Sorted(result));
+    }
+
+    [Fact]
+    public void TopKFrequent_MultipleFrequencies_ReturnsTopK()
+    {
+        var result = _sut.TopKFrequent([1, 1, 1, 2, 2, 3, 3, 3, 3], 2);
+
+        Assert.Equal([1, 3], Sorted(result));
+    }
+
+    [Fact]
+    public void TopKFrequent_AllSameFrequency_ReturnsKElements()
+    {
+        var result = _sut.TopKFrequent([1, 2, 3, 4, 5], 3);
+
+        Assert.Equal(3, result.Length);
+    }
+}
+
